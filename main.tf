f415fa61964a626a96bfcbe354fc7880c80aaab2
David Eliason <davethemaker@gmail.com>
2024-12-19 16:47:05 -0800

    add TF aws provider info and aws config and credentials files in order for VSC to access AWS services

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# configure the aws provider
+provider "aws" {
+  shared_config_files      = ["~/.aws/config"]
+  shared_credentials_files = ["~/.aws/credentials"]
+  profile                  = "vscodeaws"
+  region                   = "us-west-2"
+}
+
+
